cbfc24868d32cf0468514dd27b8972b4417daaea
TriddyKay <tristankay93@gmail.com>
2021-08-19 22:14:39 +1000

    Setting up project w/ andriod build + typescript

---
+++ App.js
@@ -0,0 +1,13 @@
+import type {Node} from 'react';
+import React from 'react';
+import {AppNavigationWrapper} from './src/components/AppNavigationWrapper';
+
+const App: () => Node = () => {
+
+  return (
+    <AppNavigationWrapper />
+  );
+};
+
+
+export default App;
